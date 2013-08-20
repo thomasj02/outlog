b10f98134ddeae2e49309414b88a89a610ca0f3d
Thomas Johnson <thomas.j.johnson@gmail.com>
2013-08-19 22:33:47 -0500

    First import of OutLog

--- outlog/msgs.py
+++ outlog/msgs.py
@@ -1,1 +1,13 @@
 __author__ = 'tjohnson'
+
+class BaseMessage(object):
+    def __init__(self, hostname, microtime, application, level, message_name):
+        self.hostname = hostname
+        self.microtime = microtime
+        self.application = application
+        self.level = level
+        self.message_name = message_name
+
+    def __repr__(self):
+        return "{microtime} {application}.{hostname} {message_name} {level}".format(**self.__dict__)
+
