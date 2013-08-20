e59528472776b17ca1900996e850f589adaa95de
Thomas Johnson <thomas.j.johnson@gmail.com>
2013-08-19 23:30:13 -0500

    Formatting

--- outlog/factory.py
+++ outlog/factory.py
@@ -3,8 +3,10 @@ import ujson
 
 __author__ = 'tjohnson'
 
+
 def get_microtime():
     return int(time.time()*1000.*1000.)
+
 
 class OutlogMessageFactory(object):
     def __init__(self, hostname, application, microtime_function=get_microtime):
@@ -17,6 +19,7 @@ class OutlogMessageFactory(object):
             hostname=self.hostname, microtime=self.microtime_function(), application=self.application,
             level=level, **kwargs)
 
+
 class OutlogJsonFileMessageFactory(OutlogMessageFactory):
     def __init__(self, hostname, application, microtime_function=get_microtime, file_handle=None):
         super(OutlogJsonFileMessageFactory, self).__init__(hostname, application, microtime_function)
@@ -26,4 +29,3 @@ class OutlogJsonFileMessageFactory(OutlogMessageFactory):
         message = super(OutlogJsonFileMessageFactory, self).msg(message_class, level, **kwargs)
         self.file_handle.write(ujson.dumps(message))
         self.file_handle.write("\n")
-
