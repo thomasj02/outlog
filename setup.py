b27580bf24ef37970cb8483a7a115bde42e5fcc3
Thomas Johnson <thomas.j.johnson@gmail.com>
2013-08-19 22:32:59 -0500

    First import of OutLog

---
+++ setup.py
@@ -0,0 +1,13 @@
+__author__ = 'tjohnson'
+
+from setuptools import setup, find_packages
+setup(
+    name = "OutLog",
+    version = "0.1",
+    packages = find_packages(),
+
+    # metadata for upload to PyPI
+    author = "Thomas Johnson",
+    author_email = "thomas.j.johnson@gmail.com",
+    description = "This is an Example Package",
+)
